83ab278bf26ab99eeecc0dda23d767fc37555b49
Luis Carrion <lcarrionalvarez@gmail.com>
2022-03-13 22:16:02 -0500

    muñeco terminado

--- src/components/Model3D.jsx
+++ src/components/Model3D.jsx
@@ -17,47 +17,55 @@ export const Model3D = () => {
     renderer.setSize(clientWidth, clientHeight);
     currentRef.appendChild(renderer.domElement);
 
-    const geometry = new THREE.BoxGeometry(0.4, 0.4, 0.4);
-    const material = new THREE.MeshBasicMaterial({color:0xffff00});
-    const cube = new THREE.Mesh(geometry, material);
-    cube.position.x = -3;
-
-    const geometryShepre = new THREE.SphereGeometry(0.3, 22, 22);
-    const materialShepre = new THREE.MeshBasicMaterial({color:0x0f2c64});
-    const Shepre = new THREE.Mesh(geometryShepre, materialShepre);
-    Shepre.position.x = 1;
-
-
-    const geometryCone = new THREE.ConeGeometry( 0.5, 0.7, 10 );
-    const materialCone = new THREE.MeshBasicMaterial( {color: 'rgb (0,100,0)'} );
-    const cone = new THREE.Mesh( geometryCone, materialCone );
-    cone.position.x = -1;
-
-    const geometryCilinder = new THREE.CylinderGeometry( 0.4, 0.4, 0.4, 20 );
-    const materialCilinder = new THREE.MeshBasicMaterial( {color: 'rgb(255, 0, 0)'} );
-    const cylinder = new THREE.Mesh( geometryCilinder, materialCilinder );
-    cylinder.position.x = 3;
+    //TODO: Figures
+    const cube = new THREE.Mesh(new THREE.BoxGeometry(2, 0.5, 0), new THREE.MeshBasicMaterial({color:0xffff00}));
     
-    scene.add(cone);
-    scene.add(cylinder);
-    scene.add(Shepre);
+    const circle1 = new THREE.Mesh(new THREE.CircleGeometry(0.2, 10), new THREE.MeshBasicMaterial({color:0xffff00}))
+    circle1.position.y = -0.47;
+    circle1.position.x = -0.8;
+
+    const circle2 = new THREE.Mesh(new THREE.CircleGeometry(0.2, 10), new THREE.MeshBasicMaterial({color:0xffff00}))
+    circle2.position.y = -0.47;
+    circle2.position.x = 0.8;
+
+    const escape = new THREE.Mesh(new THREE.BoxGeometry(0.3, 0.5, 0), new THREE.MeshBasicMaterial({color:0xffff00}))
+    escape.position.y = 0.51;
+    escape.position.x = -0.8;
+    
+    const nube1 = new THREE.Mesh(new THREE.CircleGeometry(0.03, 7), new THREE.MeshBasicMaterial({color:0xffff00}))
+    nube1.position.y = 1;
+    nube1.position.x = -0.8;
+
+    const nube2 = new THREE.Mesh(new THREE.CircleGeometry(0.07, 7), new THREE.MeshBasicMaterial({color:0xffff00}))
+    nube2.position.y = 2;
+    nube2.position.x = -0.8;
+
+    const nube3 = new THREE.Mesh(new THREE.CircleGeometry(0.1, 7), new THREE.MeshBasicMaterial({color:0xffff00}))
+    nube3.position.y = 1.5;
+    nube3.position.x = -0.8;
+
+    //TODO: add Figures
     scene.add(cube);
-    
+    scene.add(circle1);
+    scene.add(circle2);
+    scene.add(escape);
+    scene.add(nube1, nube2, nube3);
+
+    //TODO: animate
     const clock = new THREE.Clock();
     const animate = () => {
       const elapsedtime = clock.getElapsedTime();
-      cube.rotation.x = elapsedtime;
-      cube.rotation.y = elapsedtime;
-      cube.position.y = Math.sin(elapsedtime);
-      Shepre.position.y = Math.sin((elapsedtime * -1));
+      //Todo: llantas
+      circle1.rotation.z = (elapsedtime*-2);
+      circle2.rotation.z = (elapsedtime*-2);
 
-      cylinder.rotation.x = elapsedtime;
-      cylinder.rotation.y = elapsedtime;
-      cylinder.position.y = Math.sin(elapsedtime);
+      //TODO: escape
+      escape.position.y = 0.55+Math.sin(elapsedtime*5)/20;
 
-      cone.rotation.x = elapsedtime;
-      cone.rotation.y = elapsedtime;
-      cone.position.y = Math.sin(elapsedtime);
+      //TODO: nubes
+      nube1.position.y = 0.9+Math.sin(elapsedtime*-15)/30;
+      nube2.position.y = 1.2+Math.sin(elapsedtime*10)/30;
+      nube3.position.y = 1.5+Math.sin(elapsedtime*-5)/30;
 
       renderer.render(scene, camera);
       requestAnimationFrame(animate);
